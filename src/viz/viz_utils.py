0fdda93dec206c1827fc441bf68987f72ccb62d3
Collin <cjwischmeyer@gmail.com>
2025-03-09 15:27:05 -0400

    merging with tree-isolation branch: viz utils
    adding in rotating compare gif function that was previously in skeletonize (removed two commits ago)

--- src/viz/viz_utils.py
+++ src/viz/viz_utils.py
@@ -72,6 +72,107 @@ def color_continuous_map(pcd, cvar):
     density_colors = plt.get_cmap('plasma')((cvar - cvar.min()) / (cvar.max() - cvar.min()))
     density_colors = density_colors[:, :3]
     pcd.colors = o3d.utility.Vector3dVector(density_colors)
+    return pcd
+
+
+def rotating_compare_gif(transient_pcd, constant_pcd_in,
+                               init_rot: np.ndarray = np.eye(3),
+                               steps: int = 360,
+                               transient_period: int = 45,
+                               point_size: float = 1.0,
+                               output = '/code/code/pyQSM/test/',
+                               rot_center = [0,0,0],
+                               save = False,
+                               file_name = 'pcd_compare_animation'):
+        """
+            Creates a GIF comparing two point clouds. 
+        """
+        import os 
+        import time
+        from scipy.spatial.transform import Rotation as R
+        import imageio
+
+        output_folder = os.path.join(output, 'tmp')
+        print(output_folder)
+        # os.mkdir(output_folder)
+
+        # We 
+
+        # Load PCD
+        orig = deepcopy(transient_pcd)
+        # if not trans_has_color:
+        #     orig.rotate(init_rot, center=[0, 0, 0])
+
+        # skel = copy(contracted)
+        # skel.paint_uniform_color([0, 0, 1])
+        # skel.rotate(init_rot, center=[0, 0, 0])
+
+        constant_pcd = deepcopy(constant_pcd_in)
+        # constant_pcd.paint_uniform_color([0, 0, 0])
+        constant_pcd.rotate(init_rot, center=[0, 0, 0])
+
+        transient_pcd = deepcopy(orig)
+
+        vis = o3d.visualization.Visualizer()
+        vis.create_window(width=1920, height=1080)
+        vis.add_geometry(transient_pcd)
+        vis.add_geometry(constant_pcd)
+
+        ctl = vis.get_view_control()
+        ctl.set_zoom(0.6)
+
+        # Set smaller point size. Default is 5.0
+        vis.get_render_option().point_size = point_size
+        vis.get_render_option().line_width = 15
+        vis.get_render_option().light_on = False
+        vis.update_renderer()
+
+        # Calculate rotation matrix for every step. Must only be calculated once as rotations are added up in the point cloud
+        Rot_mat = R.from_euler('y', np.deg2rad(540 / steps)).as_matrix()
+
+        image_path_list = []
+
+        pcd_idx = 0
+        for i in range(steps):
+            orig.rotate(Rot_mat, center=rot_center)
+            # skel.rotate(Rot_mat, center=rot_center)
+            constant_pcd.rotate(Rot_mat, center=rot_center)
+
+            if pcd_idx == 0:
+                transient_pcd.points = orig.points
+                transient_pcd.colors = orig.colors
+                transient_pcd.normals = orig.normals
+            if pcd_idx == 1:
+                # pcd.paint_uniform_color([0, 0, 0])
+                transient_pcd.points = constant_pcd.points
+                transient_pcd.colors = constant_pcd.colors
+
+            vis.update_geometry(transient_pcd)
+            vis.update_geometry(constant_pcd)
+            vis.poll_events()
+            vis.update_renderer()
+
+            # Draw pcd for 30 frames at a time
+            #  remove for 30 between then
+            if ((i % transient_period) == 0) and i != 0:
+                pcd_idx = (pcd_idx + 1) % 2
+                
+            current_image_path = f"{output_folder}/img_{i}.jpg"
+            if save:
+                vis.capture_screen_image(current_image_path)
+                image_path_list.append(current_image_path)
+
+        vis.destroy_window()
+        images = []
+        log.info(f'Creating gif at {output_folder}{file_name}.gif')
+        if save:
+            for filename in image_path_list:
+              images.append(imageio.imread(filename))
+            log.info(f'Creating gif at {image_path_list[0]}')
+            imageio.mimsave(os.path.join(os.path.dirname(image_path_list[0]), 
+                                         '{}.gif'.format(file_name)), 
+                                         images, format='GIF')
+            
 
 # def draw_w_traj(pcd_or_data_path, 
 #                render_option_path= '',
