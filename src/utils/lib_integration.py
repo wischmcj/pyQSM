69d4d90d9050937a471809971dddf03e4e1616b2
Collin <cjwischmeyer@gmail.com>
2025-03-09 13:41:09 -0400

    optimizing find neighbors in ball to work with sets of points forming cylin

--- src/utils/lib_integration.py
+++ src/utils/lib_integration.py
@@ -5,7 +5,8 @@ import scipy.spatial as sps
 from matplotlib import pyplot as plt, patches
 
 from set_config import config, log
-from utils.math_utils import get_center, get_radius
+from utils.math_utils import get_center, get_radius, get_percentile
+from viz.viz_utils import iter_draw, draw
 
 ## Numpy
 
@@ -45,54 +46,68 @@ def sps_hull_to_mesh(voxel_down_pcd, type="ConvexHull"):
 ### KDTrees
 
 def get_neighbors_in_tree(sub_pcd_pts, full_tree, radius):
+    '''Returns indicies n_idx of points in full_tree.data such that 
+        distance(full_tree.data[n_idx],q_pt)<=radius for q_pt in sub_pcd_pts'''
     trunk_tree = sps.KDTree(sub_pcd_pts)
-    pairs = trunk_tree.query_ball_tree(full_tree, r=radius)
+    pairs = full_tree.query_ball_tree(trunk_tree, r=radius)
     neighbors = np.array(list(set(chain.from_iterable(pairs))))
     return neighbors
 
 def find_neighbors_in_ball(
     base_pts, points_to_search, 
     points_idxs, radius=None, center=None,
-    draw = False
+    use_top = None, #(85,100)
+    draw_results = False
 ):
     """
        Defines a centroid for a set of base points,
         finds all points within the search set falling
          within a sphere of a given radius centered on the centroid
     """
-    if not center:
-        center = get_center(base_pts)
-    if not radius:
-        radius = get_radius(base_pts) * config['sphere']["radius_multiplier"]
+    if use_top:
+        top_pt_idxs,_ = get_percentile(base_pts,use_top[0],use_top[1])
+        top_pts = base_pts[top_pt_idxs]
+        if not center:
+            center = get_center(top_pts)
+            center = [center[0], center[1], max(base_pts[:, 2])] 
+        if not radius:
+            radius = get_radius(top_pts) * config['sphere']["radius_multiplier"]
+    else:
+        if not center:
+            center = get_center(base_pts)
+        if not radius:
+            radius = get_radius(base_pts) * config['sphere']["radius_multiplier"]
 
     if radius < config['sphere']["min_radius"]:
         radius = config['sphere']["min_radius"]
     if radius > config['sphere']["max_radius"]:
         radius = config['sphere']["max_radius"]
-    log.info(f"{radius=}")
-
-    center = [center[0], center[1], max(base_pts[:, 2])]  # - (radius/4)])
+    log.info(f" Finding nbrs in ball w/ {radius=}, {center=}")
 
     full_tree = sps.KDTree(points_to_search)
     neighbors = full_tree.query_ball_point(center, r=radius)
     res = []
-    if draw:
+    if draw_results:
         ax = plt.figure().add_subplot(projection='3d')
         ax.scatter(base_pts[:,0], base_pts[:,1], base_pts[:,2], 'r')
     for results in neighbors:
         new_points = np.setdiff1d(results, points_idxs)
         res.extend(new_points)
-        if draw:
+        if draw_results:
             nearby_points = points_to_search[new_points]
             ax.plot(nearby_points[:,0], nearby_points[:,1], nearby_points[:,2], 'o')
     sphere =  o3d.geometry.TriangleMesh.create_sphere(radius=radius)
     sphere.translate(center)
-    if draw:
-        sphere_pts=sphere.sample_points_uniformly(500)
-        sphere_pts.paint_uniform_color([0,1,0])
-        ax.plot(sphere_pts[:,0], sphere_pts[:,1], sphere_pts[:,2], 'o')
-        plt.show()
-    return sphere, res
+    if draw_results:
+        # sphere_pts=sphere.sample_points_uniformly(500)
+        # sphere_pts.paint_uniform_color([0,1,0])
+        # sphere_pts = np.array(sphere_pts.points)
+        # ax.plot(sphere_pts[:,0], sphere_pts[:,1], sphere_pts[:,2], 'o')
+        # plt.show()
+        test = o3d.geometry.PointCloud()
+        test.points = o3d.utility.Vector3dVector(base_pts)
+        draw([sphere, test])
+    return sphere, neighbors, center, radius
 
 ## Matplotlib
 
